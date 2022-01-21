50fb8efed82d607a4d75f172802f469cdf2aa5f7
Luiz Calazans <luizfernando.calazans8@gmail.com>
2022-01-20 22:44:58 -0300

    Create purchase entity and controller

---
+++ src/main/kotlin/com/mercadolivro/model/Purchase.kt
@@ -0,0 +1,36 @@
+package com.mercadolivro.model
+
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity(name = "purchase")
+@Table(name = "purchase")
+data class Purchase(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    val customer: Customer,
+
+    @ManyToMany
+    @JoinTable(
+        name = "purchase_book",
+        joinColumns = [JoinColumn(name = "purchase_id")],
+        inverseJoinColumns = [JoinColumn(name = "book_id")]
+    )
+    val books: List<Book>,
+
+    @Column
+    val nfe: String? = null,
+
+    @Column
+    val price: BigDecimal,
+
+    @Column(name = "created_at")
+    val createdAt: LocalDateTime
+
+
+)
